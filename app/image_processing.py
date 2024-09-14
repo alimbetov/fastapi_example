aa1b1e600ffef4d1c8a83842840bec0b6f21bad9
alyumi <lanton.1999@mail.ru>
2024-09-14 22:06:12 +0500

    fix: files saving

--- app/image_processing.py
+++ app/image_processing.py
@@ -8,6 +8,11 @@ import shutil
 import os
 
 DIR_FOR_UNPROCESSED_IMAGES = "/unprocessed/"
+UPLOAD_DIRECTORY = "uploaded_images/" 
+
+if not os.path.exists(f'{UPLOAD_DIRECTORY}{DIR_FOR_UNPROCESSED_IMAGES}'):
+        os.makedirs(f'{UPLOAD_DIRECTORY}{DIR_FOR_UNPROCESSED_IMAGES}')
+
 
 # Функция обработки изображения с использованием Tesseract
 def process_image(image_path: str) -> str:
@@ -16,11 +21,19 @@ def process_image(image_path: str) -> str:
 
     # Преобразование изображения в градации серого
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    convertMatrixIntoImageThenSave(f'{image_path}_gray_scale', matrix=gray)
+    convertMatrixIntoImageThenSave(get_filename_for_image(image_path, '_gray_scale'), matrix=gray)
 
-    # Применение гауссовского размытия для уменьшения шумов
+    # _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+    # contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # cv2.drawContours(gray, contours, -1, (0, 255, 0), 2)
+    # convertMatrixIntoImageThenSave(get_filename_for_image(image_path, '_contour'), matrix=gray)
+
+    # # Применение гауссовского размытия для уменьшения шумов
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    convertMatrixIntoImageThenSave(f'{image_path}_blur', matrix=blurred)
+    convertMatrixIntoImageThenSave(get_filename_for_image(image_path, '_blur'), matrix=blurred)
+
 
     # Применение Tesseract для распознавания текста
     text = pytesseract.image_to_string(blurred, config='--psm 7')
@@ -28,17 +41,17 @@ def process_image(image_path: str) -> str:
     return text
 
 
-def convertMatrixIntoImageThenSave(orginal_file_path, matrix):
+def convertMatrixIntoImageThenSave(filepath, matrix):
+    # Save the matrix as an image
+    if (cv2.imwrite(filepath, matrix) == True):
+        print(f'{filepath} is saved')
+    else:
+        print(f'{filepath} is not saved')
+
+
+def get_filename_for_image(orginal_file_path, image_type):
     file_directory = os.path.dirname(orginal_file_path)
     filename = os.path.basename(orginal_file_path)
+    clean_filename, _ = os.path.splitext(filename)
 
-    if not os.path.exists(f'{file_directory}{DIR_FOR_UNPROCESSED_IMAGES}'):
-        os.makedirs(f'{file_directory}{DIR_FOR_UNPROCESSED_IMAGES}')
-
-    file_path = f"{file_directory}{DIR_FOR_UNPROCESSED_IMAGES}{filename}.png"
-
-    # Save the matrix as an image
-    if (cv2.imwrite(file_path, matrix) == True):
-        print(f'{file_path} is saved')
-    else:
-        print(f'{file_path} is not saved')
+    return f"{file_directory}{DIR_FOR_UNPROCESSED_IMAGES}{clean_filename}{image_type}.png"
